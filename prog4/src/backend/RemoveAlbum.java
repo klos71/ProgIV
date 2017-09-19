c399cedb8b78d5e4b06ac83f7d777104d93cd53d
RivarRoffs <ivarforss@hotmail.com>
2017-09-20 00:24:23 +0300

    album remove

--- prog4/src/backend/RemoveAlbum.java
+++ prog4/src/backend/RemoveAlbum.java
@@ -3,7 +3,8 @@ package backend;
 public class RemoveAlbum {
 
     public void excecute(Album anAlbum){
-        
+        if (!anAlbum.getParent().equals(null)){
+            anAlbum.getParent().getChildren().remove(anAlbum);
         }
     }
 }
